e36877386f522f5ed66fc2f8f224c077d2904e31
Faheem <fhm3120@gmail.comm>
2018-04-29 00:15:12 +0530

    initial commit

---
+++ test_senti_mod.py
@@ -0,0 +1,8 @@
+import sentiment_mod as s
+
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
+print(s.sentiment("This movie is an average! The acting was worse, plot was average, and there were nothing"))
+print(s.sentiment("This movie was of poor quality"))
+print(s.sentiment("This movie was low standard and nothing is good in this."))
+print(s.sentiment("This movie is fantastic and i love it"))
